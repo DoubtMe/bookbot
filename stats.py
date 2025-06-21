63e225556cf718144292dcb84d87ae2d3e2bb83d
DoubtMe <platinumdemigod86@gmail.com>
2025-06-21 14:50:44 -0400

    Updates

--- stats.py
+++ stats.py
@@ -2,5 +2,15 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def character_count():
-    pass
+def character_count(text):
+    characters = {}
+    for letter in text:
+        if letter.lower() in characters:
+            characters[letter.lower()] += 1
+            
+        else:
+            characters[letter.lower()] = 1
+    return characters
+        
+
+
