eddd01dfecf909a30ff4322aa49e365b496dee14
DoubtMe <platinumdemigod86@gmail.com>
2025-06-30 22:04:41 -0400

    Completed program

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import sys
 from stats import get_num_words, character_count, sort_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    dict = character_count(text)
-    num_words = get_num_words(text)
-    print(f"============ BOOKBOT ============\nAnalysing book found at {book_path}\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------")
-    # This doesn't work
-    sorted_dict = sort_dict(dict)
-    for item in sorted_dict:
-       if item['char'].isalpha():
-            print(f"{item['char']}: {item['num']}")
-    print("============= END ===============")
+    arguments = len(sys.argv)
+    if arguments != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        dict = character_count(text)
+        num_words = get_num_words(text)
+        print(f"============ BOOKBOT ============\nAnalysing book found at {book_path}\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------")
+        sorted_dict = sort_dict(dict)
+        for item in sorted_dict:
+            if item['char'].isalpha():
+                    print(f"{item['char']}: {item['num']}")
+        print("============= END ===============")
     
     
 
